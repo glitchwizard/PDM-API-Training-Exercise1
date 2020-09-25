6c494d25f4b0b792b1fcd50ec34538f75473e859
glitchwizard <glitchwizard@gmail.com>
2020-09-24 19:02:51 -0600

    Initial Commit

---
+++ PDM-API-Excercise1/Form1.Designer.vb
@@ -0,0 +1,75 @@
+﻿<Global.Microsoft.VisualBasic.CompilerServices.DesignerGenerated()> _
+Partial Class Exercise1
+    Inherits System.Windows.Forms.Form
+
+    'Form overrides dispose to clean up the component list.
+    <System.Diagnostics.DebuggerNonUserCode()> _
+    Protected Overrides Sub Dispose(ByVal disposing As Boolean)
+        Try
+            If disposing AndAlso components IsNot Nothing Then
+                components.Dispose()
+            End If
+        Finally
+            MyBase.Dispose(disposing)
+        End Try
+    End Sub
+
+    'Required by the Windows Form Designer
+    Private components As System.ComponentModel.IContainer
+
+    'NOTE: The following procedure is required by the Windows Form Designer
+    'It can be modified using the Windows Form Designer.  
+    'Do not modify it using the code editor.
+    <System.Diagnostics.DebuggerStepThrough()> _
+    Private Sub InitializeComponent()
+        Me.VaultsComboBox = New System.Windows.Forms.ComboBox()
+        Me.LoginButton = New System.Windows.Forms.Button()
+        Me.Label1 = New System.Windows.Forms.Label()
+        Me.SuspendLayout()
+        '
+        'VaultsComboBox
+        '
+        Me.VaultsComboBox.FormattingEnabled = True
+        Me.VaultsComboBox.Location = New System.Drawing.Point(30, 33)
+        Me.VaultsComboBox.Name = "VaultsComboBox"
+        Me.VaultsComboBox.Size = New System.Drawing.Size(164, 21)
+        Me.VaultsComboBox.TabIndex = 0
+        '
+        'LoginButton
+        '
+        Me.LoginButton.Cursor = System.Windows.Forms.Cursors.Default
+        Me.LoginButton.Location = New System.Drawing.Point(33, 64)
+        Me.LoginButton.Name = "LoginButton"
+        Me.LoginButton.Size = New System.Drawing.Size(158, 49)
+        Me.LoginButton.TabIndex = 1
+        Me.LoginButton.Text = "Log in and Display Vault Info"
+        Me.LoginButton.UseVisualStyleBackColor = True
+        '
+        'Label1
+        '
+        Me.Label1.AutoSize = True
+        Me.Label1.Location = New System.Drawing.Point(44, 10)
+        Me.Label1.Name = "Label1"
+        Me.Label1.Size = New System.Drawing.Size(136, 13)
+        Me.Label1.TabIndex = 2
+        Me.Label1.Text = "Select Preferred Vault View"
+        '
+        'Exercise1
+        '
+        Me.AutoScaleDimensions = New System.Drawing.SizeF(6.0!, 13.0!)
+        Me.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font
+        Me.ClientSize = New System.Drawing.Size(225, 131)
+        Me.Controls.Add(Me.Label1)
+        Me.Controls.Add(Me.LoginButton)
+        Me.Controls.Add(Me.VaultsComboBox)
+        Me.Name = "Exercise1"
+        Me.Text = "Exercise1"
+        Me.ResumeLayout(False)
+        Me.PerformLayout()
+
+    End Sub
+
+    Friend WithEvents VaultsComboBox As ComboBox
+    Friend WithEvents LoginButton As Button
+    Friend WithEvents Label1 As Label
+End Class
---
+++ PDM-API-Excercise1/Form1.vb
@@ -0,0 +1,33 @@
+﻿Imports EPDM.Interop.epdm
+Imports EPDM.Interop.EPDMResultCode
+
+
+Public Class Exercise1
+
+    Private Sub LoginButton_Click(sender As Object, e As EventArgs) Handles LoginButton.Click
+        Dim vault As IEdmVault5 = New EdmVault5()
+
+        Dim VaultMajorVersion As Integer
+        Dim VaultMinorVersion As Integer
+
+        MsgBox("Solidworks PDM Version: " + VaultMajorVersion + "." + VaultMinorVersion)
+
+        vault.LoginAuto(VaultsComboBox.Text, Me.Handle.ToInt32())
+
+        MsgBox("We are " + IIf(vault.IsLoggedIn, "", " not") + " logged in to " + VaultsComboBox.Text + ".")
+
+    End Sub
+
+    Private Sub Exercise1_Load(sender As Object, e As EventArgs) Handles MyBase.Load
+        Dim vault As IEdmVault8 = New EdmVault5
+        Dim Views() As EdmViewInfo = {}
+        vault.GetVaultViews(Views, False)
+        VaultsComboBox.Items.Clear()
+        For Each View As EdmViewInfo In Views
+            VaultsComboBox.Items.Add(View.mbsVaultName)
+        Next
+        If VaultsComboBox.Items.Count > 0 Then
+            VaultsComboBox.Text = VaultsComboBox.Items(0)
+        End If
+    End Sub
+End Class
